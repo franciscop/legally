2bc55bbe2a92106e20a49d223f1822d066087bbb
Bob Tolbert <bob@eyesopen.com>
2017-01-14 08:46:36 -0700

    Fix package search to handle @types packages
    Typescript now installs type declarations in subdirectories under
    node_modules/@types. This adds code to correctly include these
    packages when building the list as well as when printing the
    packages titles in the report.
    
    Also adds a small change to work on Node on Windows. The way node
    recursively call node.cli results in a couple of extra arguments in
    opt._ that we need to remove. This may be a bug in minimist but
    seems pretty easy to work around.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var analysis = require('./lib/analysis');
 var remote = require('./lib/remote');
 var opt = require('minimist')(process.argv.filter(e => !/\/.+$/.test(e)));
 
+var isWin = /^win/.test(process.platform);
+
 opt.show = opt.show || [];
 opt.show = opt.show instanceof Array ? opt.show : [opt.show];
 
@@ -28,6 +30,11 @@ opt.type = opt.type instanceof Array ? opt.type : [opt.type];
 opt.width = opt.width || 80;
 opt.width = typeof opt.width === 'number' ? opt.width : 80;
 
+// node on windows inserts extra into argv that we need to remove
+// could be a bug in minimist
+if (isWin) {
+  opt._ = opt._.slice(2);
+}
 
 var licenses;
 if (opt._.length) {
