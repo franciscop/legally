cfcc2e594661772b2f05eb70ef47b1bf2b4bb5f4
Francisco Presencia <public@francisco.io>
2018-10-04 09:33:07 +0900

    Fixing AppVeyor bug

--- index.test.js
+++ index.test.js
@@ -9,8 +9,9 @@ describe('legally', () => {
 
   it('baconjs: folder called "readme"', async () => {
     const bacon = await legally('baconjs');
-    const [name, licenses] = Object.entries(bacon)[0];
+    const name = Object.keys(bacon)[0];
     expect(name.split('@')[0]).toBe('baconjs');
+    const licenses = Object.values(bacon)[0];
     expect(Array.isArray(licenses.package)).toBe(true);
     expect(Array.isArray(licenses.copying)).toBe(true);
     expect(Array.isArray(licenses.readme)).toBe(true);
